10340869502ba6f1f7495ebc030b6a62e060bb7f
code-enginner <sina.us.2019@gmail.com>
2017-07-05 13:18:24 +0430

    index file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    require_once 'configs/configs.php';
+    require_once root.'helperFunctions/helper.php';
+
+?>
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="">
+    <script type="text/javascript" src=""></script>
+    <script type="text/javascript" src=""></script>
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<?php
+
+?>
+
+
+
+<script type="text/javascript" src=""></script>
+<script type="text/javascript" src=""></script>
+</body>
+</html>
